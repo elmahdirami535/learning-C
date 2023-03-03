da9bc24a95c2d6f493bd23e0ac82e24a511e99e1
root <root@Matebook.localdomain>
2023-03-03 05:24:05 +0100

    i add the code finally and it works well

---
+++ c_project_3/index.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * this is a comment 
+ *
+ * yeah brother
+ *
+ * what's up
+ */
+
+int main(void)
+{
+	what();
+}
---
+++ c_project_3/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+
+int what(void)
+{
+	printf("what the fuck are youd doing here\n");
+	return (0);
+}
