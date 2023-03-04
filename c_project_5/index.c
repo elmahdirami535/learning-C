936d10cabf050facec7de09bdd8accf99a94fa83
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-04 19:08:10 +0100

    commit

---
+++ c_project_5/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *this is a comment
+ *
+ *yeey it's annoying
+ *
+ * return 0
+ *
+ * main have two value
+ */
+
+int main(void)
+{
+	printf("hello, world");
+	return (0);
+}
