071cf91fd41c6a5c5862d9d240f2102d188818eb
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-05 05:14:06 +0100

    commit

--- c_project_5/main.c
+++ c_project_5/main.c
@@ -21,22 +21,22 @@ int cause(void)
 int passweord(void)
 {
 	int pin = 0;
-  int tries = 0;
+	int tries = 0;
 
-  printf("BANK OF CODECADEMY:\n");
-  printf("Enter your PIN: ");
-  scanf("%d", &pin);
+	printf("BANK OF THE WORLD:\n");
+	printf("Enter your PIN: ");
+	scanf("%d", &pin);
 
-  tries++;
+	tries++;
 
-  while (pin != 1234 && tries < 3) {
-    printf("Enter your PIN: ");
-    scanf("%d", &pin);
-    tries++;
-  }
+	while (pin != 200556 && tries < 3) {
+		printf("Enter your PIN: ");
+		scanf("%d", &pin);
+		tries++;
+	}
 
-  if (pin == 1234) {
-    printf("PIN accepted!\n");
-    printf("You now have access.\n");
-  }
+	if (pin == 200556) {
+		printf("PIN accepted!\n");
+		printf("You now have access.\n");
+	}
 }
