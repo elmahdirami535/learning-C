c38d9c01c7e0f4d0060de75d027946e22bbfb027
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-07 22:58:11 +0100

    nice

--- pointer/omar.c
+++ pointer/omar.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-/**
- * main - Entry point
- *
- * Description: print value of n satus; zero, positive or negative
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
 	int value;
@@ -20,28 +12,28 @@ int main(void)
 	printf("What values do you have ?\n");
 	printf("If you have the radius, print 1\n");
 	printf("If you have the diameter, print 2.\n");
-	scanf(" %d ", &value);
+	scanf("%d", &value);
 
 	if (value == 2)
 	{
 		printf("What is your circle diameter?\n");
-		scanf(" %f ", &d);
+		scanf("%f", &d);
 
 		r = 0.5 * d;
 		float circumfrence = 2 * PI * r;
 
-		printf("\nYour circle's circumference is %.2f.", circumfrence);
+		printf("\nYour circle's circumference is %.2f. \n", circumfrence);
 	}
 
 	else if (value == 1)
 	{
 
 		printf("What is your circle radius?\n");
-		scanf(" \n%f ", &r);
+		scanf("%f", &r);
 
 		float circumfrence = 2 * PI * r;
 
-		printf("\nYour circle's circumference is %.2f.", circumfrence);
+		printf("\nYour circle's circumference is %.2f. \n", circumfrence);
 	}
 
 	return (0);
--- pointer/omg.c
+++ pointer/omg.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<string.h>
+include<string.h>
 
 int main() {
   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
