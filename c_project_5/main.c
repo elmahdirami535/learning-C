a5aa7c3f53cddc2800ceb6f58b6c08aedfabd666
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-07 05:23:01 +0100

    i add a project

--- c_project_5/main.c
+++ c_project_5/main.c
@@ -62,3 +62,4 @@ int lopie(void)
 		is--;
 	}
 }
+
---
+++ c_project_6/index.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int mat2[][3] = {{1, 6, 3}, {5, 9, 2}};
+	printf("this is a module %i", mat2[0][2]);
+	return (0);
+}
