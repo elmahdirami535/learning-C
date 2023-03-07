943152aa1c487471f7ca4c008af76aed2ea960a1
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-07 14:13:02 +0100

    h

--- pointer/omg.c
+++ pointer/omg.c
@@ -9,9 +9,9 @@ int main() {
   // Code for Checkpoint 1 goes here.
 int *ptr = &arr[9];
   // Code for Checkpoint 2 goes here.
-for (int i = 0 ; i < arr[9] ; i++)
+for (int i = 0 ; i < arr[] ; i++)
 {
-printf("%p\n", *ptr);
+printf("%p\n", &ptr);
 
 }
   // Code for Checkpoint 3 goes here.
