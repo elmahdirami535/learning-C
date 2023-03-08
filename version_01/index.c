c03014c7c3b628f171b51256bc750cca6ad9c480
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-08 07:18:58 +0100

    finish

--- version_01/index.c
+++ version_01/index.c
@@ -7,29 +7,33 @@
  * Description: print value of n satus; zero, positive or negative
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	float r = 0.5, circumfrence = 2;
-	float PI = 3.14;
+	float r[4] = {0.5, 2, 3.14};
 	int value  = get_int("Lets calculate the circle's circumf \n What value do you have ? \n If you have the radius, print 1\n If you have diameter , print 2\n");
-	switch (value)
+	do
 	{
-		case 2:
-			float d = get_float("What is your circle diameter?\n ");
-			r *= d;
-			circumfrence *= PI * r;
-			printf("\n Your circle's circumference is %.2f.", circumfrence);
-			break;
-		case 1:
-			r = get_float("What is your circle radius?\n ");
-			circumfrence *= PI * r;
-			printf("\nYour circle's circumference is %.2f.", circumfrence);
-			break;
-		default:
-			printf("Invalid\n");
-			break;
+		switch (value)
+		{
+			case 2:
+				r[3] = get_float("What is your circle diameter?\n");
+				r[0] *= r[3];
+				r[1] *= r[2] * r[0];
+				printf("\n Your circle's circumference is %.2f ", r[1]);
+				break;
+			case 1:
+				r[0] = get_float("What is your circle radius?\n");
+				r[1] *= r[2] * r[0];
+				printf("\nYour circle's circumference is %.2f ", r[1]);
+				break;
+			default:
+				printf("choose just two numbers 1 or 2");
+				break;
+		}
 	}
+	while ( 2 != value || value != 1 );
+
 	return (0);
 }
