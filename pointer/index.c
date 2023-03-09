1510b48257b5425bb5965e843c36649327a6e493
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-09 16:06:08 +0100

    .

--- pointer/index.c
+++ pointer/index.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int i = 16; 
 	int *pointer = &i;
-                     
+                      
 	printf("%p\n", &i); //&i is the address of the variable i
 	printf("%p\n", pointer); //pointer is the value stored in the pointer pointer, which is also the address of the variable i
 	printf("%p\n", *pointer); //*pointer is the value pointed to by the pointer pointer, which is the value of the variable i
