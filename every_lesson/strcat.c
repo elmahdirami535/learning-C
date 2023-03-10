80fc9f7b6ac2167fd12810237e8368d5058d74ca
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-10 21:51:29 +0100

    omg

--- every_lesson/strcat.c
+++ every_lesson/strcat.c
@@ -9,10 +9,18 @@
 
 
 /**
- * main - Entry point
- *
- * Description: print value of n satus; zero, positive or negative
- *
- * Return: Always 0 (Success)
+ * strcat: concatenates two strings, source string and destination string.
+ * It copies the source string at the end of the destination string, adds a
+ * null terminator and returns the destination string. Note that the destination
+ * string must have enough space available to accommodate both the existing contents
+ * and the copy of the source string.
 */
 
+int main() 
+{
+   char str1[15] = "Hello, ";
+   char str2[15] = "world!";
+   strcat(str1, str2);
+   printf("%s\n", str1);
+   return 0;
+}
