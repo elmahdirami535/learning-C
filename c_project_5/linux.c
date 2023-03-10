c6ee1e75e105b813c7cc0fa9952d70ed687af72c
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-10 19:18:19 +0100

    n

---
+++ c_project_5/linux.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf(this is a module);
+	return (0);
+}
