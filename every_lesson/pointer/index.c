1e722f1316d401061b6ff9bda4f61b3a2d24f0e0
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-03-10 19:38:10 +0100

    omg

---
+++ every_lesson/pointer/index.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 16; 
+	int *pointer = &i;
+
+	printf("%p\n", &i); //&i is the address of the variable i
+	printf("%p\n", pointer); //pointer is the value stored in the pointer pointer, which is also the address of the variable i
+	printf("%p\n", *pointer); //*pointer is the value pointed to by the pointer pointer, which is the value of the variable i
+	printf("%p\n", &pointer); //&pointer is the address of the pointer pointer. 
+}
